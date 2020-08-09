08e903d5417205bdfd822e49fbfacb0d76e6cfa9
LatvianPython <43447170+latvianpython@users.noreply.github.com>
2020-08-09 17:43:37 +0200

    added new fixture to have less code in individual tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 
+from jira_dump import Dumper
+
 
 @pytest.fixture
 def patch_jira(monkeypatch):
@@ -12,13 +14,17 @@ def patch_jira(monkeypatch):
             pass
 
         def _get_json(self, path, params, base):
-            with open('./test_data/sample_sla.json', mode='r', encoding='utf-8') as file:
+            with open(
+                "./test_data/sample_sla.json", mode="r", encoding="utf-8"
+            ) as file:
                 test_sla = json.loads(file.read())
 
             return test_sla
 
         def worklogs(self, issue):
-            with open('./test_data/sample_worklog.json', mode='r', encoding='utf-8') as file:
+            with open(
+                "./test_data/sample_worklog.json", mode="r", encoding="utf-8"
+            ) as file:
                 raw_worklog = json.loads(file.read())
                 test_worklog = Worklog(options=None, session=None, raw=raw_worklog)
 
@@ -26,13 +32,21 @@ def patch_jira(monkeypatch):
 
         def search_issues(self, *args, **kwargs):
             _ = args, self
-            if kwargs['startAt'] > 0:
+            if kwargs["startAt"] > 0:
                 return []
 
-            with open('./test_data/sample_issue.json', mode='r', encoding='utf-8') as file:
+            with open(
+                "./test_data/sample_issue.json", mode="r", encoding="utf-8"
+            ) as file:
                 raw_issue = json.loads(file.read())
                 test_issue = Issue(options=None, session=None, raw=raw_issue)
 
             return [test_issue]
 
-    monkeypatch.setattr(jira, 'JIRA', MockJIRA)
+    monkeypatch.setattr(jira, "JIRA", MockJIRA)
+
+
+@pytest.fixture
+def dumper(patch_jira):
+    with Dumper(server="https://jira.server.com") as _dumper:
+        yield _dumper
--- tests/test_base.py
+++ tests/test_base.py
@@ -6,14 +6,13 @@ from jira_dump import Dumper, IssueField
 from jira_dump.base import dict_value, get_fields, extract_dict
 
 
-def test_dumper_basic(patch_jira):
-    with Dumper(server="https://jira.server.com") as dumper:
-        issues = list(dumper.issues)
-        assert len(issues) == 1
+def test_dumper_basic(dumper):
+    issues = list(dumper.issues)
+    assert len(issues) == 1
 
-        issue = issues[0]
-        assert issue["status"] == "Running automatic tests"
-        assert issue["issue"] == "TEST-42"
+    issue = issues[0]
+    assert issue["status"] == "Running automatic tests"
+    assert issue["issue"] == "TEST-42"
 
 
 def test_subclassing(patch_jira):
@@ -56,72 +55,66 @@ def test_extract_dict():
     assert parsed_issue["c"] == "b"
 
 
-def test_worklogs(patch_jira):
-    with Dumper(server="https://jira.server.com") as dumper:
-        worklogs = list(dumper.worklogs)
+def test_worklogs(dumper):
+    worklogs = list(dumper.worklogs)
 
-        assert len(worklogs) == 10
-        assert worklogs[0]["author"] == "john.doe"
-        assert sorted(worklogs[0].keys()) == [
-            "author",
-            "comment",
-            "issue",
-            "started",
-            "time_spent",
-        ]
-
-
-def test_transitions(patch_jira):
-    with Dumper(server="https://jira.server.com") as dumper:
-        transitions = list(dumper.transitions)
-
-        assert len(transitions) == 3
-
-        transition = transitions[0]
-        assert transition["author"] == "john@server.com"
-        assert list(transition.keys()) == ["author", "created", "from", "to", "issue"]
+    assert len(worklogs) == 10
+    assert worklogs[0]["author"] == "john.doe"
+    assert sorted(worklogs[0].keys()) == [
+        "author",
+        "comment",
+        "issue",
+        "started",
+        "time_spent",
+    ]
 
 
-def test_comments(patch_jira):
-    with Dumper(server="https://jira.server.com") as dumper:
-        comments = list(dumper.comments)
+def test_transitions(dumper):
+    transitions = list(dumper.transitions)
 
-        assert len(comments) == 1
+    assert len(transitions) == 3
 
-        comment = comments[0]
-        assert comment["author"] == "jane.doe@server.com"
-        assert sorted(list(comment.keys())) == sorted(
-            ["author", "created", "body", "issue"]
-        )
+    transition = transitions[0]
+    assert transition["author"] == "john@server.com"
+    assert list(transition.keys()) == ["author", "created", "from", "to", "issue"]
 
 
-def test_fix_versions(patch_jira):
-    with Dumper(server="https://jira.server.com") as dumper:
-        fix_versions = list(dumper.fix_versions)
+def test_comments(dumper):
+    comments = list(dumper.comments)
 
-        assert len(fix_versions) == 1
+    assert len(comments) == 1
 
-        fix_version = fix_versions[0]
-        assert fix_version["name"] == "RELEASE_05"
-        assert sorted(list(fix_version.keys())) == sorted(
-            ["description", "name", "release_date", "issue"]
-        )
-
-
-def test_sla_overview(patch_jira):
-    with Dumper(server="https://jira.server.com") as dumper:
-        sla_overview = list(dumper.sla_overview)
-
-        assert len(sla_overview) == 2
-
-        sla = sla_overview[0]
-        assert sla["status"] == "SUCCESS"
+    comment = comments[0]
+    assert comment["author"] == "jane.doe@server.com"
+    assert sorted(list(comment.keys())) == sorted(
+        ["author", "created", "body", "issue"]
+    )
 
 
-def test_dataframes(patch_jira):
-    with Dumper(server="https://jira.server.com") as dumper:
-        for name, object_ in inspect.getmembers(Dumper):
-            if "__" not in name and inspect.isdatadescriptor(object_):
-                df = pd.DataFrame(getattr(dumper, name))
-                assert "issue" in df.columns
-                assert len(df) > 0
+def test_fix_versions(dumper):
+    fix_versions = list(dumper.fix_versions)
+
+    assert len(fix_versions) == 1
+
+    fix_version = fix_versions[0]
+    assert fix_version["name"] == "RELEASE_05"
+    assert sorted(list(fix_version.keys())) == sorted(
+        ["description", "name", "release_date", "issue"]
+    )
+
+
+def test_sla_overview(dumper):
+    sla_overview = list(dumper.sla_overview)
+
+    assert len(sla_overview) == 2
+
+    sla = sla_overview[0]
+    assert sla["status"] == "SUCCESS"
+
+
+def test_dataframes(dumper):
+    for name, object_ in inspect.getmembers(Dumper):
+        if "__" not in name and inspect.isdatadescriptor(object_):
+            df = pd.DataFrame(getattr(dumper, name))
+            assert "issue" in df.columns
+            assert len(df) > 0
